e04c31762ace80a001c4aca0279d7ea4b089a3d3
Arek <Solaris1995@gmail.com>
2016-11-01 16:37:33 +0100

    Added class and createGems

--- js/project-x.js
+++ js/project-x.js
@@ -1,4 +1,32 @@
 /* global document:true */
 document.addEventListener(`DOMContentLoaded`, () => {
 
+	class Board {
+		constructor(_size, _numberOfGemTypes){
+			this.size = _size;
+			this.numberOfGemTypes = _numberOfGemTypes;
+			this.numberOfGems = Math.pow(_size, 2);
+			this.arrayOfGems = [ ];
+			this.createGems();
+		}
+		createGems(){
+			for(let i = 0; i < this.numberOfGems; i += 1){
+				this.arrayOfGems[i] = {
+					type: null,
+					imageSrc: null,
+					points: null,
+					position: [null,null],
+				}
+			}
+			return this;
+		};
+
+	}
+	function createNewBoard() {
+		newBoard = new Board(8,6);
+	}
+	createNewBoard();
+
 });
+
+
