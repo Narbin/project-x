cc88c91104495c2d848305e9939b8023f1bff47b
Arek <Solaris1995@gmail.com>
2016-11-01 17:48:16 +0100

    Added new images, created randomTypeOfGem and added positionXY

--- js/project-x.js
+++ js/project-x.js
@@ -2,28 +2,45 @@
 document.addEventListener(`DOMContentLoaded`, () => {
 
 	class Board {
-		constructor(_size, _numberOfGemTypes){
+		constructor(_size, _numberOfGemTypes, _typeOfBundle){
 			this.size = _size;
 			this.numberOfGemTypes = _numberOfGemTypes;
 			this.numberOfGems = Math.pow(_size, 2);
 			this.arrayOfGems = [ ];
+			this.typeOfBundle = _typeOfBundle;
 			this.createGems();
 		}
+
 		createGems(){
+			let positionY = 0;
 			for(let i = 0; i < this.numberOfGems; i += 1){
+				if(i%this.size === 0){
+					positionY += 1;
+				}
 				this.arrayOfGems[i] = {
-					type: null,
+					type: this.randomTypeOfGem(this.typeOfBundle),
 					imageSrc: null,
 					points: null,
-					position: [null,null],
+					positionXY: [i%this.size,positionY-1],
 				}
 			}
 			return this;
 		};
 
+		randomTypeOfGem(bundle){
+			let bundleObject = [ ]; 
+			switch(bundle) {
+				case `fruits`:
+					bundleObject = [`banana`,`cherry`,`pear`,`pineapple`,`raspberry`,`strawberry`];
+				default:
+					bundleObject = [`banana`,`cherry`,`pear`,`pineapple`,`raspberry`,`strawberry`];
+			}
+			return bundleObject[Math.floor(Math.random()*bundleObject.length)];
+		};
+
 	}
 	function createNewBoard() {
-		newBoard = new Board(8,6);
+		newBoard = new Board(8,6,`fruits`);
 	}
 	createNewBoard();
 
